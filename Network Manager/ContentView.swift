8d3df3cb25a4d024db75446acf6bda5b27145839
Alp Uney <alpuney@windowslive.com>
2021-11-28 14:36:39 +0300

    Initial Commit

---
+++ Network Manager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Network Manager
+//
+//  Created by Alp Üney on 28.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Network Manager/Network_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_ManagerApp.swift
+//  Network Manager
+//
+//  Created by Alp Üney on 28.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Network_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
